d98ef11a2be2f35b910f07048a03cdd00750b0bb
Bajcur <130090609+Bajcur@users.noreply.github.com>
2023-08-25 22:23:01 +0200

    Upgrades password confirmation feature
    This commit allows user to enter "confirm password" first then "password" and it will also check if the passwords are the same.

--- script.js
+++ script.js
@@ -11,5 +11,15 @@ confirmedPassword.addEventListener("input", () => {
         confirmedPassword.setCustomValidity("Passwords have to be the same");
     } else {
         confirmedPassword.setCustomValidity("");
+        password.setCustomValidity("");
+    }
+});
+
+password.addEventListener("input", () => {
+    if(confirmedPassword.value !== password.value){
+        password.setCustomValidity("Passwords have to be the same");
+    } else {
+        password.setCustomValidity("");
+        confirmedPassword.setCustomValidity("");
     }
 });
